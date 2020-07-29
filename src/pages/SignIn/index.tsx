d3cbe011b8cd348c3e5b27e30fbf34fcc29557c7
GileardeFernandes <gileardefernandes@gmail.com>
2020-07-29 19:10:47 -0300

    first commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { Container, Title } from './style';
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
+};
+
+export default SignIn;
